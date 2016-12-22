fcecd5328d66570ced0cb43979a1d8e7fa776170
Ahmed Khan <khan.ahmedraza@gmail.com>
2016-12-22 14:59:09 -0500

    new changes for the project

---
+++ src/main/java/com/javabuddy/JavabuddyApplication.java
@@ -0,0 +1,12 @@
+package com.javabuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavabuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavabuddyApplication.class, args);
+	}
+}
