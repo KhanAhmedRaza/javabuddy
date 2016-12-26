4faf9a6d26e74ad70c83289f380234e15f1d4cb8
Ahmed Khan <khan.ahmedraza@gmail.com>
2016-12-26 12:56:46 -0500

    adding new files to repository

---
+++ src/main/java/com/javabuddy/web/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package com.javabuddy.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping("/")
+	public String home(){
+		return "index";
+	}
+
+}
