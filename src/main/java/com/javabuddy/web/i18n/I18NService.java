ba0bf57b4da7691e7a6740e2146e00fcbacbd324
Ahmed Khan <khan.ahmedraza@gmail.com>
2016-12-28 15:08:05 -0500

    added logging feature

--- src/main/java/com/javabuddy/web/i18n/I18NService.java
+++ src/main/java/com/javabuddy/web/i18n/I18NService.java
@@ -2,6 +2,8 @@ package com.javabuddy.web.i18n;
 
 import java.util.Locale;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -9,11 +11,13 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class I18NService {
+	private static final Logger LOG = LoggerFactory.getLogger(I18NService.class);
 	
 	@Autowired
 	private MessageSource messageSource;
 	
 	public String getMessage(String messageId){
+		LOG.info("Returning 1i18n for message id {}", messageId);
 		Locale locale = LocaleContextHolder.getLocale();
 		return getMessage(messageId,locale);
 	}
